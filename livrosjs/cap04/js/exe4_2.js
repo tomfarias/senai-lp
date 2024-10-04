8590e8fbc8db1555c0a708b6975006d2cdb37d58
welington <tomfarias87@gmail.com>
2024-10-03 21:17:35 -0300

    pagina index

--- livrosjs/cap04/js/exe4_2.js
+++ livrosjs/cap04/js/exe4_2.js
@@ -1,15 +1,24 @@
 function calcular() {
-    alert();
-    //const nome document.getElementById("inNome").value;
-    const masculino = document.getElementById("inMasculino").checked;
-    const altura = document.getElementById("inAltura").value;
 
-    let peso;
-    if (masculino) {
+    const nome =  document.getElementById("inNome").value;
+    const masculino = document.getElementById("inMasculino").checked;
+    
+    const altura = document.getElementById("inAltura").value;
+    
+    let peso = masculino ? Math.pow(altura, 2) * 22 : Math.pow(altura, 2) * 21;
+    /*if (masculino) {
         peso = 22 * Math.pow(altura, 2);
     } else {
         peso = 21 * Math.pow(altura, 2);
-    }
-    document.querySelector("h3").innerText = "Peso ideal: " + peso.toFixed(2);
+    }*/
+
+    
+    document.querySelector("h3").innerText = `${nome} peso ideal ${peso.toFixed(2)};`;
+
     return false; // vai impedir o refresh da pagina equivalente ao e.preventDefault()
+
+}
+
+function limpar() {
+    document.querySelector("h3").innerText = "";
 }
